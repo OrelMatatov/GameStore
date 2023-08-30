ca1fcc50a6e94f7ac9fd118ec3d82487f245e5eb
Orel Matatov <orel.matatov@gmail.com>
2023-08-30 17:27:10 +0300

    Create deletedGames.js

---
+++ models/deletedGames.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DeletedGame = new Schema({
+    gameId: {
+        type: Schema.Types.ObjectId,
+        required: true 
+    },
+    
+    title: {
+        type: String,
+        required: true
+    },
+
+    platform: {
+        type: String,
+        required: true
+    },
+
+    price: {
+        type: Number,
+        required: true
+    },
+
+    about: {
+        type: String
+    },
+
+    rating: {
+        type: Number,
+        default: 0
+    },
+
+    releaseYear: {
+        type: Number,
+        default: 2023
+    },
+
+    supplier: {
+        type: Schema.Types.ObjectId,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('DeletedGame', DeletedGame)
